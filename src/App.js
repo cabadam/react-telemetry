897dfc1360aa7a2c10058a4f4075695f9a7d4fb1
Adam Clauss <cabadam@gmail.com>
2024-08-08 16:53:14 -0600

    It works!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = async () => {
+    setCount(count + 1);
+    await fetch('http://localhost:3000/', { mode: 'no-cors' });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleClick}>App name is {process.env.REACT_APP_NAME}.  Count is {count}</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
